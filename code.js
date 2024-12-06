47979ed2b84880dcc9c4c4d34e47ff3787125530
TC <nothing@nothing.com>
2024-12-06 17:30:42 +1300

    Add some custom validation messages

--- code.js
+++ code.js
@@ -33,3 +33,44 @@ hedgehogs.forEach( hedgehog => {
 
 // Set to best hedgehog :)
 hSelectInput.childNodes[3].selected = true;
+
+
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+  console.log(event);
+})
+
+const firstNameInput = document.querySelector('#first-name');
+const lastNameInput = document.querySelector('#last-name');
+const emailInput = document.querySelector('#email');
+const passwordInput = document.querySelector('#password');
+const confirmPasswordInput = document.querySelector('#confirm-password');
+
+const submitBut = document.querySelector('button');
+submitBut.addEventListener('click', e => {
+
+  if (!firstNameInput.checkValidity()) {
+    firstNameInput.nextElementSibling.textContent = `Please enter a name between ${firstNameInput.getAttribute('minlength')} and ${firstNameInput.getAttribute('maxlength')} characters`;
+  } else {
+    firstNameInput.nextElementSibling.textContent = '';
+  }
+
+  if (!lastNameInput.checkValidity()) {
+    lastNameInput.nextElementSibling.textContent = `Please enter a name between ${lastNameInput.getAttribute('minlength')} and ${lastNameInput.getAttribute('maxlength')} characters`;
+  } else {
+    lastNameInput.nextElementSibling.textContent = '';
+  }
+  
+  if (!emailInput.checkValidity()) {
+    emailInput.nextElementSibling.textContent = 'Please enter valid email like john.doe@ILoveHedgehogs.com'
+  } else {
+    emailInput.nextElementSibling.textContent = '';
+  }
+  
+  if (passwordInput.value !== confirmPasswordInput.value) {
+    passwordInput.nextElementSibling.textContent = 'Passwords must match';
+  } else {
+    passwordInput.nextElementSibling.textContent = ''
+  }
+})
+
